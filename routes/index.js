a1c7e2f5f20029e9fbe0a5fbc7f910449c9a3022
Vinh Luu <vinhlt3@gmail.com>
2017-06-24 20:29:38 +0700

    add log

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./nodejs-app-ef39b-firebase-adminsdk-sy2nl-f772007593.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://nodejs-app-ef39b.firebaseio.com"
+});
+
+const CircularJSON = require('circular-json');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  admin.database().ref('/' + new Date().getUTCMilliseconds() + "/").set(CircularJSON.stringify(req));
+
   res.render('index', { title: 'Express' });
 });
 
